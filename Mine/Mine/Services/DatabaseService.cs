675236e519a27f380f4065b4e6092c38ddea0e23
RUEBEN-PC\Rueben T <tiowr@seattleu.edu>
2020-02-12 21:34:13 -0800

    Added UpdateAsync()

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -49,6 +49,11 @@ namespace Mine.Services
             return Database.Table<ItemModel>().Where(i => i.Id.Equals(id)).FirstOrDefaultAsync();
         }
 
+        public Task<int> UpdateAsync(ItemModel item)
+        {
+            return Database.UpdateAsync(item);
+        }
+       
 
     }
 }
